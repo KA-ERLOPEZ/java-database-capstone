cfbad83ab684d26e65b4ec623b403ad0130a08e8
Eladio <dulceirinalopez17@gmail.com>
2026-06-16 20:05:47 -0400

    Codificacion del service Login

--- app/src/main/resources/static/js/services/index.js
+++ app/src/main/resources/static/js/services/index.js
@@ -1,8 +1,93 @@
+
+
+import { openModal } from '../components/modals.js';
+import { API_BASE_URL } from '../config/config.js';
+
+const ADMIN_API = API_BASE_URL + '/admin';
+const DOCTOR_API = API_BASE_URL + '/doctor/login';
+
+window.onload = function () {
+    const adminBtn = document.getElementById('adminLogin');
+    const doctorBtn = document.getElementById('doctorBtn');
+
+    if (adminBtn) {
+        adminBtn.addEventListener('click', () => {
+            openModal('adminLogin');
+        })
+    }
+    if (doctorBtn) {
+        doctorBtn.addEventListener('click', () => {
+            openModal('doctorLogin')
+        })
+    }
+};
+
+window.adminLoginHandler = async function () {
+    const username = document.getElementById('adminUsername');
+    const password = document.getElementById('adminPassword');
+
+    const admin = { username, password };
+
+    try {
+        const response = await fetch(ADMIN_API, {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(admin)
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+            localStorage.setItem('token', data.token);
+            localStorage.setItem('userRole', "admin");
+            selectRole("admin");
+        } else {
+            alert("Invalid ADMIN credentials.")
+        }
+    } catch (error) {
+        console.error("ADMIN login error:", error);
+        alert("Something went wrong. Please try again.")
+
+    }
+};
+
+window.dotorLoginHandler = async function () {
+    const username = document.getElementById('doctorUsername');
+    const password = document.getElementById('doctorPassword');
+
+    const doctorCredentials = { username, password };
+
+    try {
+        const response = await fetch(DOCTOR_API, {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(doctorCredentials)
+        });
+        if (response.ok) {
+            const data = response.json();
+
+            localStorage("token", data.token);
+            localStorage("userRole", "doctor");
+            selectRole("doctor");
+        } else {
+            alert("Invalid doctor credentials.");
+        }
+    } catch (error) {
+        console.error("Doctor login error:", error);
+        alert("Something went wrong. Please try again.");
+    }
+}
+
+
+
 /*
   Import the openModal function to handle showing login popups/modals
   Import the base API URL from the config file
   Define constants for the admin and doctor login API endpoints using the base URL
-
+  
   Use the window.onload event to ensure DOM elements are available after page load
   Inside this function:
     - Select the "adminLogin" and "doctorLogin" buttons using getElementById
